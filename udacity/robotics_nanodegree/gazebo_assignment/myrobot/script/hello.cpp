bbd6bd2d0fc67a25808412d905fedc21f640d05f
karamach <karamach@github.com>
2021-02-16 21:00:36 +0000

    gazebo assignment

---
+++ udacity/robotics_nanodegree/gazebo_assignment/myrobot/script/hello.cpp
@@ -0,0 +1,19 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+    class WorldPluginMyRobot: public WorldPlugin
+    {
+    public:
+        WorldPluginMyRobot(): WorldPlugin()
+        {
+            printf("Welcome to karthik's world \n");
+        }
+
+      void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+        {
+        }
+    };
+
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
